b2e7d876c3dc9c752c7af030533bc2c7746c4506
Jerryco Araes <araesjerryco@gmail.com>
2025-03-25 08:49:12 +0800

    added

--- Araes_IT13A_activities/midterm/basic_calculator.java
+++ Araes_IT13A_activities/midterm/basic_calculator.java
@@ -2,47 +2,47 @@
 
 package midterm;
 
-import java.util.Scanner;
-
-
 public class basic_calculator {
-     
+
+    public static int Addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int Subraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int Multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int Division(int a, int b) {
+        return a / b;
+    }
+
+    public static float Module(float a, float b ) {
+        return (float) a/ b*100;
+
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        
-        System.out.print("Enter first number: ");
-        double x = scanner.nextDouble();
-      
-        System.out.print("Enter first number: ");
-        char y = scanner.next(). charAt(0);
-        
-        System.out.print("Enter first number: ");
-        double z = scanner.nextDouble();
-        
-        double result = 0;
-        boolean valid = true;
-        
-        if (y == '+') result = x + z;
-        else if (y == '-')result = x - z;
-        else if (y == '*')result = x * z;
-        else if (y == '/') {
-              
-        if (z !=0) result = (x / z);
-               else{System.out.println("Cannotdivide by zero!"); valid = false; }
+        int result = Addition(3, 10);
+        System.out.println("Sum: " + result);
+
+        int result2 = Subraction(8, 10);
+        System.out.println("Difference: " + result2);
+
+        int result3 = Multiplication(2, 10);
+        System.out.println("Product: " + result3);
+
+        int result4 = Division(10, 10);
+        System.out.println("Quotient: " + result4);
+
+        float result5 = Module (90, 60);
+        System.out.println("Mod: " + result5);
     }
-        else if (y == '%') result = (x * z) / 100;
-        
-                else {System.out.println("invalid operator!"); valid = false; }
-        
-        if (valid)System.out.println("Result: " + result);
-        
-        scanner.close();
-        
-    }
-    }
-        
-        
-        
+}
+   
         
         
         
